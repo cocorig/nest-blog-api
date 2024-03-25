c4eda5b6f78067738ed07742568119430f0d7bae
Hayeon LEE <fhdodl@naver.com>
2024-03-26 03:46:57 +0900

    db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60, limit: 10 }]),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+
+    // feature module
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT;
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe({}));
+  app.enableVersioning({ type: VersioningType.URI });
+  app.use(helmet());
+  app.use(compression());
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Application running at port ${PORT}`);
+  });
 }
 bootstrap();
